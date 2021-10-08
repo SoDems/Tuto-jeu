cae00714adbb20218605bd89dc845bd00dd6c73b
SoDems <desmarissonia@gmail.com>
2021-10-08 10:21:20 +0200

    wip: css

---
+++ app.js
@@ -0,0 +1,24 @@
+//But du jeu : trouver un nombre aléatoire entre 0 et 100 
+//Système de vie
+//Système de chaud froid
+//Manipulation du DOM en JS
+
+//On peut trouver le nombre aléatoire dans la console
+
+//plan
+    // - Mise en place 
+    // - HTML
+    // - CSS 
+    // - Début de JS 
+            // -Logique globale JS : ce qui se passe qd on charge la page web 
+            // - Système chaud / froid
+            // - Système de Vie 
+            // - Bouton recommencer 
+
+// 1) mise place
+//     - vscode
+//     - extension : color highlight
+//                   ES Lint 
+
+
+                  
